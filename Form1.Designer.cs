232825606f4a61930b6d84dcfc5adc6ce3566f7c
tranjul2 <Crystal_Apple2@yahoo.com>
2022-05-30 21:16:24 -0700

    Updated Form Designer

---
+++ Form1.Designer.cs
@@ -0,0 +1,356 @@
+﻿namespace BingoGarme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.A1 = new System.Windows.Forms.Button();
+            this.A2 = new System.Windows.Forms.Button();
+            this.A3 = new System.Windows.Forms.Button();
+            this.A4 = new System.Windows.Forms.Button();
+            this.A5 = new System.Windows.Forms.Button();
+            this.A6 = new System.Windows.Forms.Button();
+            this.A7 = new System.Windows.Forms.Button();
+            this.A8 = new System.Windows.Forms.Button();
+            this.A9 = new System.Windows.Forms.Button();
+            this.A10 = new System.Windows.Forms.Button();
+            this.A11 = new System.Windows.Forms.Button();
+            this.A12 = new System.Windows.Forms.Button();
+            this.A13 = new System.Windows.Forms.Button();
+            this.A14 = new System.Windows.Forms.Button();
+            this.A15 = new System.Windows.Forms.Button();
+            this.A25 = new System.Windows.Forms.Button();
+            this.A24 = new System.Windows.Forms.Button();
+            this.A23 = new System.Windows.Forms.Button();
+            this.A22 = new System.Windows.Forms.Button();
+            this.A21 = new System.Windows.Forms.Button();
+            this.A20 = new System.Windows.Forms.Button();
+            this.A19 = new System.Windows.Forms.Button();
+            this.A18 = new System.Windows.Forms.Button();
+            this.A17 = new System.Windows.Forms.Button();
+            this.A16 = new System.Windows.Forms.Button();
+            this.btnReplay = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // A1
+            // 
+            this.A1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A1.Location = new System.Drawing.Point(21, 12);
+            this.A1.Name = "A1";
+            this.A1.Size = new System.Drawing.Size(127, 128);
+            this.A1.TabIndex = 0;
+            this.A1.UseVisualStyleBackColor = true;
+            this.A1.Click += new System.EventHandler(this.A1_Click);
+            // 
+            // A2
+            // 
+            this.A2.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A2.Location = new System.Drawing.Point(154, 12);
+            this.A2.Name = "A2";
+            this.A2.Size = new System.Drawing.Size(127, 128);
+            this.A2.TabIndex = 1;
+            this.A2.UseVisualStyleBackColor = true;
+            this.A2.Click += new System.EventHandler(this.A2_Click);
+            // 
+            // A3
+            // 
+            this.A3.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A3.Location = new System.Drawing.Point(287, 12);
+            this.A3.Name = "A3";
+            this.A3.Size = new System.Drawing.Size(127, 128);
+            this.A3.TabIndex = 2;
+            this.A3.UseVisualStyleBackColor = true;
+            this.A3.Click += new System.EventHandler(this.A3_Click);
+            // 
+            // A4
+            // 
+            this.A4.Location = new System.Drawing.Point(420, 12);
+            this.A4.Name = "A4";
+            this.A4.Size = new System.Drawing.Size(127, 128);
+            this.A4.TabIndex = 3;
+            this.A4.UseVisualStyleBackColor = true;
+            // 
+            // A5
+            // 
+            this.A5.Location = new System.Drawing.Point(553, 12);
+            this.A5.Name = "A5";
+            this.A5.Size = new System.Drawing.Size(127, 128);
+            this.A5.TabIndex = 4;
+            this.A5.UseVisualStyleBackColor = true;
+            // 
+            // A6
+            // 
+            this.A6.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A6.Location = new System.Drawing.Point(21, 146);
+            this.A6.Name = "A6";
+            this.A6.Size = new System.Drawing.Size(127, 128);
+            this.A6.TabIndex = 5;
+            this.A6.UseVisualStyleBackColor = true;
+            this.A6.Click += new System.EventHandler(this.A6_Click);
+            // 
+            // A7
+            // 
+            this.A7.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A7.Location = new System.Drawing.Point(154, 146);
+            this.A7.Name = "A7";
+            this.A7.Size = new System.Drawing.Size(127, 128);
+            this.A7.TabIndex = 6;
+            this.A7.UseVisualStyleBackColor = true;
+            this.A7.Click += new System.EventHandler(this.A7_Click);
+            // 
+            // A8
+            // 
+            this.A8.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A8.Location = new System.Drawing.Point(287, 146);
+            this.A8.Name = "A8";
+            this.A8.Size = new System.Drawing.Size(127, 128);
+            this.A8.TabIndex = 7;
+            this.A8.UseVisualStyleBackColor = true;
+            this.A8.Click += new System.EventHandler(this.A8_Click);
+            // 
+            // A9
+            // 
+            this.A9.Location = new System.Drawing.Point(420, 146);
+            this.A9.Name = "A9";
+            this.A9.Size = new System.Drawing.Size(127, 128);
+            this.A9.TabIndex = 8;
+            this.A9.UseVisualStyleBackColor = true;
+            // 
+            // A10
+            // 
+            this.A10.Location = new System.Drawing.Point(553, 146);
+            this.A10.Name = "A10";
+            this.A10.Size = new System.Drawing.Size(127, 128);
+            this.A10.TabIndex = 9;
+            this.A10.UseVisualStyleBackColor = true;
+            // 
+            // A11
+            // 
+            this.A11.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A11.Location = new System.Drawing.Point(21, 280);
+            this.A11.Name = "A11";
+            this.A11.Size = new System.Drawing.Size(127, 128);
+            this.A11.TabIndex = 10;
+            this.A11.UseVisualStyleBackColor = true;
+            this.A11.Click += new System.EventHandler(this.A11_Click);
+            // 
+            // A12
+            // 
+            this.A12.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A12.Location = new System.Drawing.Point(154, 280);
+            this.A12.Name = "A12";
+            this.A12.Size = new System.Drawing.Size(127, 128);
+            this.A12.TabIndex = 11;
+            this.A12.UseVisualStyleBackColor = true;
+            this.A12.Click += new System.EventHandler(this.A12_Click);
+            // 
+            // A13
+            // 
+            this.A13.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A13.Location = new System.Drawing.Point(287, 280);
+            this.A13.Name = "A13";
+            this.A13.Size = new System.Drawing.Size(127, 128);
+            this.A13.TabIndex = 12;
+            this.A13.UseVisualStyleBackColor = true;
+            this.A13.Click += new System.EventHandler(this.A13_Click);
+            // 
+            // A14
+            // 
+            this.A14.Location = new System.Drawing.Point(420, 280);
+            this.A14.Name = "A14";
+            this.A14.Size = new System.Drawing.Size(127, 128);
+            this.A14.TabIndex = 13;
+            this.A14.UseVisualStyleBackColor = true;
+            // 
+            // A15
+            // 
+            this.A15.Location = new System.Drawing.Point(553, 280);
+            this.A15.Name = "A15";
+            this.A15.Size = new System.Drawing.Size(127, 128);
+            this.A15.TabIndex = 14;
+            this.A15.UseVisualStyleBackColor = true;
+            // 
+            // A25
+            // 
+            this.A25.Location = new System.Drawing.Point(553, 548);
+            this.A25.Name = "A25";
+            this.A25.Size = new System.Drawing.Size(127, 128);
+            this.A25.TabIndex = 24;
+            this.A25.UseVisualStyleBackColor = true;
+            // 
+            // A24
+            // 
+            this.A24.Location = new System.Drawing.Point(420, 548);
+            this.A24.Name = "A24";
+            this.A24.Size = new System.Drawing.Size(127, 128);
+            this.A24.TabIndex = 23;
+            this.A24.UseVisualStyleBackColor = true;
+            // 
+            // A23
+            // 
+            this.A23.Location = new System.Drawing.Point(287, 548);
+            this.A23.Name = "A23";
+            this.A23.Size = new System.Drawing.Size(127, 128);
+            this.A23.TabIndex = 22;
+            this.A23.UseVisualStyleBackColor = true;
+            // 
+            // A22
+            // 
+            this.A22.Location = new System.Drawing.Point(154, 548);
+            this.A22.Name = "A22";
+            this.A22.Size = new System.Drawing.Size(127, 128);
+            this.A22.TabIndex = 21;
+            this.A22.UseVisualStyleBackColor = true;
+            // 
+            // A21
+            // 
+            this.A21.Location = new System.Drawing.Point(21, 548);
+            this.A21.Name = "A21";
+            this.A21.Size = new System.Drawing.Size(127, 128);
+            this.A21.TabIndex = 20;
+            this.A21.UseVisualStyleBackColor = true;
+            // 
+            // A20
+            // 
+            this.A20.Location = new System.Drawing.Point(553, 414);
+            this.A20.Name = "A20";
+            this.A20.Size = new System.Drawing.Size(127, 128);
+            this.A20.TabIndex = 19;
+            this.A20.UseVisualStyleBackColor = true;
+            // 
+            // A19
+            // 
+            this.A19.Location = new System.Drawing.Point(420, 414);
+            this.A19.Name = "A19";
+            this.A19.Size = new System.Drawing.Size(127, 128);
+            this.A19.TabIndex = 18;
+            this.A19.UseVisualStyleBackColor = true;
+            // 
+            // A18
+            // 
+            this.A18.Location = new System.Drawing.Point(287, 414);
+            this.A18.Name = "A18";
+            this.A18.Size = new System.Drawing.Size(127, 128);
+            this.A18.TabIndex = 17;
+            this.A18.UseVisualStyleBackColor = true;
+            // 
+            // A17
+            // 
+            this.A17.Location = new System.Drawing.Point(154, 414);
+            this.A17.Name = "A17";
+            this.A17.Size = new System.Drawing.Size(127, 128);
+            this.A17.TabIndex = 16;
+            this.A17.UseVisualStyleBackColor = true;
+            // 
+            // A16
+            // 
+            this.A16.Location = new System.Drawing.Point(21, 414);
+            this.A16.Name = "A16";
+            this.A16.Size = new System.Drawing.Size(127, 128);
+            this.A16.TabIndex = 15;
+            this.A16.UseVisualStyleBackColor = true;
+            // 
+            // btnReplay
+            // 
+            this.btnReplay.Location = new System.Drawing.Point(747, 83);
+            this.btnReplay.Name = "btnReplay";
+            this.btnReplay.Size = new System.Drawing.Size(123, 57);
+            this.btnReplay.TabIndex = 25;
+            this.btnReplay.Text = "Replay";
+            this.btnReplay.UseVisualStyleBackColor = true;
+            this.btnReplay.Click += new System.EventHandler(this.btnReplay_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(922, 793);
+            this.Controls.Add(this.btnReplay);
+            this.Controls.Add(this.A25);
+            this.Controls.Add(this.A24);
+            this.Controls.Add(this.A23);
+            this.Controls.Add(this.A22);
+            this.Controls.Add(this.A21);
+            this.Controls.Add(this.A20);
+            this.Controls.Add(this.A19);
+            this.Controls.Add(this.A18);
+            this.Controls.Add(this.A17);
+            this.Controls.Add(this.A16);
+            this.Controls.Add(this.A15);
+            this.Controls.Add(this.A14);
+            this.Controls.Add(this.A13);
+            this.Controls.Add(this.A12);
+            this.Controls.Add(this.A11);
+            this.Controls.Add(this.A10);
+            this.Controls.Add(this.A9);
+            this.Controls.Add(this.A8);
+            this.Controls.Add(this.A7);
+            this.Controls.Add(this.A6);
+            this.Controls.Add(this.A5);
+            this.Controls.Add(this.A4);
+            this.Controls.Add(this.A3);
+            this.Controls.Add(this.A2);
+            this.Controls.Add(this.A1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "BINGO GAME";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button A1;
+        private System.Windows.Forms.Button A2;
+        private System.Windows.Forms.Button A3;
+        private System.Windows.Forms.Button A4;
+        private System.Windows.Forms.Button A5;
+        private System.Windows.Forms.Button A6;
+        private System.Windows.Forms.Button A7;
+        private System.Windows.Forms.Button A8;
+        private System.Windows.Forms.Button A9;
+        private System.Windows.Forms.Button A10;
+        private System.Windows.Forms.Button A11;
+        private System.Windows.Forms.Button A12;
+        private System.Windows.Forms.Button A13;
+        private System.Windows.Forms.Button A14;
+        private System.Windows.Forms.Button A15;
+        private System.Windows.Forms.Button A25;
+        private System.Windows.Forms.Button A24;
+        private System.Windows.Forms.Button A23;
+        private System.Windows.Forms.Button A22;
+        private System.Windows.Forms.Button A21;
+        private System.Windows.Forms.Button A20;
+        private System.Windows.Forms.Button A19;
+        private System.Windows.Forms.Button A18;
+        private System.Windows.Forms.Button A17;
+        private System.Windows.Forms.Button A16;
+        private System.Windows.Forms.Button btnReplay;
+    }
+}
+
